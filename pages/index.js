a62d9b74099f1c58dfddc085e965666e1c68f35a
dwightreyes17 <111841503+dwightreyes17@users.noreply.github.com>
2022-09-17 11:57:12 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,97 @@ export default function Home() {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>Simple Blog Site</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm <b>Dwight Reyes</b>. I'm a Computer Enginnering student from University of the Assumption.I love playing basketball or watching movies or videos about basketball on my free time, I love seafoods and those dishes who has veggies (nakakasawa na po kasi kapag laging meat) and I want to be a pilot someday and have my own business.
-. You can contact me on <Link href="https://www.facebook.com/dwight.michael10">Facebook.</Link></p>
-        <p>
+
+      <p><b>Contact Information:</b></p>
+
+      <p>Address: Blk 16 lot 22, Fiesta Community, Brgy. Sabanilla, Mexico, Pampanga</p>
+
+      <p>Cellphone: +63956 954 7765 </p>
+
+      <p>Email Address: dwightmichael.017@gmail.com </p>
+
+      <p>Facebook Link: <Link href="https://www.facebook.com/dwight.michael10">Dwight Reyes</Link></p>
+
+      <p><b>Description:</b></p>
+
+      <p>To secure a responsible career opportunity to fully utilize my training and skills, while making a significant contribution to the success of the company.. </p>
+
+      <p><b>Skills:</b></p>
+
+      <li>Knowledgeable in C++, Python, and JavaScript programming languages.</li>
+      <li>Oriented in HTML5 .</li>
+      <li>Knowledgeable in Microsoft Offices such as Microsoft Word, Excel and PowerPoint. </li>
+      <li>Capable creating Infographics. </li>
+      <li>Time Management. </li>
+      <li>Communication Skills . </li>
+
+      <p><b>Educational Attainment:</b></p>
+      <div>
+        <p style={{textAlign: "left"}}><b>Tertiary Level</b></p>
+        <p style={{textAlign: "center"}}>
+        Bachelor of Science in Computer Engineering (Degree)
+        </p>
+        <p style={{textAlign: "center"}}>
+        University of the Assumption
+        </p>
+        <p style={{textAlign: "center"}}>
+        Unisite Subdivision, Barangay Del Pilar, 
+        </p>
+        <p style={{textAlign: "center"}}>
+        City of San Fernando Pampanga 
+        </p>
+        <p style={{textAlign: "center"}}>
+        2019-Present
+        </p>
+      </div>
+
+      <div>
+        <p style={{textAlign: "left"}}><b>Secondary Level</b></p>
+        <p style={{textAlign: "center"}}>
+        Pampanga High School
+        </p>
+        <p style={{textAlign: "center"}}>
+        High School Blvd, Lourdes, San Fernando, Pampanga
+        </p>
+        <p style={{textAlign: "center"}}>
+        Unisite Subdivision, Barangay Del Pilar, 
+        </p>
+        <p style={{textAlign: "center"}}>
+        2017-2019 (Senior High School)
+        </p>
+        <br></br>
+        <p style={{textAlign: "center"}}>
+         Pampanga High School, Inc.
+        </p>
+        <p style={{textAlign: "center"}}>
+        High School Blvd, Lourdes, San Fernando, Pampanga
+        </p>
+        <p style={{textAlign: "center"}}>
+        2014-2017 (Junior High School) 
+        </p>
+        
+      </div>
+
+      <div>
+        <p style={{textAlign: "left"}}><b>Primary Level</b></p>
+        <p style={{textAlign: "center"}}>
+        San Fernando Elementary School
+        </p>
+        <p style={{textAlign: "center"}}>
+        129 B. Mendoza, San Fernando, 2000 Pampanga 
+        </p>
+        <p style={{textAlign: "center"}}>
+        City of San Fernando, Pampanga
+        </p>
+        <p style={{textAlign: "center"}}>
+        2006-2013
+        </p>
+      </div>
+      <br></br>
+        <p style={{textAlign: "left"}}>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
